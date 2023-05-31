0ae651f4187a630dc3e0bb0b25e4acf303cbc29e
shaunchuang <72535297+shaunchuang@users.noreply.github.com>
2023-05-31 22:50:41 +0800

    Initial Commit

---
+++ firstApp/firstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppApp.swift
+//  firstApp
+//
+//  Created by Shaun Chuang on 2023/5/31.
+//
+
+import SwiftUI
+
+@main
+struct firstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
